87fd95934a73954318dd956f616e5e93fc9a670e
Sai Sumanth Goud Parakala <saisumanth.p@tadigital.com>
2022-04-28 02:54:42 +0530

    testing commitlint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
